70bb3c27a6a378748195d29adc39fd02dbd591ef
davidl-222 <david.lin27@ycdsbk12.ca>
2025-01-06 09:06:06 -0500

    Update FinalCPTDavid.java

--- FinalCPTDavid.java
+++ FinalCPTDavid.java
@@ -2,6 +2,13 @@ import arc.*;
 
 public class FinalCPTDavid {
 	public static void main(String[] args) {
-		con.println("hello world");
+		Console con = new Console();
+		con.println("Press P to Play");
+		con.println();
+		con.println("Press S to View High Score");
+		con.println();
+		con.println("Press Q to Quit");
+		con.println();
+		con.println("Press H for Help");
 	}
 }
