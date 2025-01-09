9e0d43617b1f07cb16741fdbde54564508f73abc
davidl-222 <david.lin27@ycdsbk12.ca>
2025-01-09 09:26:08 -0500

    jan 9

--- FinalCPTDavid.java
+++ FinalCPTDavid.java
@@ -17,6 +17,7 @@ public class FinalCPTDavid {
 			con.clear();
 			con.println("What is your name?");
 			String strName = con.readLine();
+			con.println();
 			
 			TextInputFile themes = new TextInputFile("themes.txt");
 			while (themes.eof() == false) {
@@ -25,7 +26,31 @@ public class FinalCPTDavid {
 			}
 			con.println("What theme do you want to play?");
 			String strPlayTheme = con.readLine();
+			con.clear();
 			
+			String[][] strWords = new String[14][2];
+			if (strPlayTheme.equalsIgnoreCase("baseball")) {
+				TextInputFile baseball = new TextInputFile("baseball.txt");
+				for (int intCount = 0; intCount < 14; intCount++) {
+					strWords[intCount][0] = baseball.readLine();
+					strWords[intCount][1] = "" + (int) (Math.random() * 14 + 1);
+				}
+			}
+			String strTempName;
+			String strTempNum;
+			for (int intCount = 0; intCount < 13; intCount++) {
+				for (int i = 0; i < 13 - intCount; i++) {
+					if (Integer.parseInt(strWords[i][1]) > Integer.parseInt(strWords[i+1][1])) {
+						strTempName = strWords[i][0];
+						strTempNum = strWords[i][1];
+						strWords[i][0] = strWords[i+1][0];
+						strWords[i][1] = strWords[i+1][1];
+						strWords[i+1][0] = strTempName;
+						strWords[i+1][1] = strTempNum;
+					}
+				}
+			}
+			for (int i = 0; i < 14; i++) con.println(strWords[i][0] + " " + strWords[i][1]);
 		}
 	}
 }
