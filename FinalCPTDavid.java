82fd6d25d3a9d427a2397687514be304ee0f09e3
davidl-222 <david.lin27@ycdsbk12.ca>
2025-01-13 09:35:35 -0500

    jan 13

--- FinalCPTDavid.java
+++ FinalCPTDavid.java
@@ -13,47 +13,60 @@ public class FinalCPTDavid {
 		con.println();
 		
 		char chrChoice = con.readChar();
+		boolean blnQuit = false;
 		if (chrChoice == 'p') {
 			con.clear();
 			con.println("What is your name?");
 			String strName = con.readLine();
 			con.println();
 			
-			TextInputFile themes = new TextInputFile("themes.txt");
-			while (themes.eof() == false) {
-				String strTheme = themes.readLine();
-				con.println(strTheme);
-			}
-			con.println("What theme do you want to play?");
-			String strPlayTheme = con.readLine();
-			con.clear();
-			
-			String[][] strWords = new String[14][2];
-			if (strPlayTheme.equalsIgnoreCase("baseball")) {
-				TextInputFile baseball = new TextInputFile("baseball.txt");
-				for (int intCount = 0; intCount < 14; intCount++) {
-					strWords[intCount][0] = baseball.readLine();
-					strWords[intCount][1] = "" + (int) (Math.random() * 100 + 1);
+			while (blnQuit == false) {
+				TextInputFile themes = new TextInputFile("themes.txt");
+				while (themes.eof() == false) {
+					String strTheme = themes.readLine();
+					con.println(strTheme);
 				}
-			}
-			String strTempName;
-			String strTempNum;
-			for (int intCount = 0; intCount < 13; intCount++) {
-				for (int i = 0; i < 13 - intCount; i++) {
-					if (Integer.parseInt(strWords[i][1]) > Integer.parseInt(strWords[i+1][1])) {
-						strTempName = strWords[i][0];
-						strTempNum = strWords[i][1];
-						strWords[i][0] = strWords[i+1][0];
-						strWords[i][1] = strWords[i+1][1];
-						strWords[i+1][0] = strTempName;
-						strWords[i+1][1] = strTempNum;
+				con.println("What theme do you want to play?");
+				String strPlayTheme = con.readLine();
+				con.clear();
+				
+				String[][] strWords = new String[14][2];
+				if (strPlayTheme.equalsIgnoreCase("baseball")) {
+					TextInputFile baseball = new TextInputFile("baseball.txt");
+					for (int intCount = 0; intCount < 14; intCount++) {
+						strWords[intCount][0] = baseball.readLine();
+						strWords[intCount][1] = "" + (int) (Math.random() * 100 + 1);
+					}
+				} else if (strPlayTheme.equalsIgnoreCase("skiing")) {
+					TextInputFile skiing = new TextInputFile("skiing.txt");
+					for (int intCount = 0; intCount < 14; intCount++) {
+						strWords[intCount][0] = skiing.readLine();
+						strWords[intCount][1] = "" + (int) (Math.random() * 100 + 1);
+					}
+				} else if (strPlayTheme.equalsIgnoreCase("foods")) {
+					TextInputFile foods = new TextInputFile("foods.txt");
+					for (int intCount = 0; intCount < 14; intCount++) {
+						strWords[intCount][0] = foods.readLine();
+						strWords[intCount][1] = "" + (int) (Math.random() * 100 + 1);
 					}
 				}
+				String strTempName;
+				String strTempNum;
+				for (int intCount = 0; intCount < 13; intCount++) {
+					for (int i = 0; i < 13 - intCount; i++) {
+						if (Integer.parseInt(strWords[i][1]) > Integer.parseInt(strWords[i+1][1])) {
+							strTempName = strWords[i][0];
+							strTempNum = strWords[i][1];
+							strWords[i][0] = strWords[i+1][0];
+							strWords[i][1] = strWords[i+1][1];
+							strWords[i+1][0] = strTempName;
+							strWords[i+1][1] = strTempNum;
+						}
+					}
+				}
+				String strSecret = strWords[0][0];
+				char[] scramble = new char[strSecret.length()];
 			}
-			String strSecret = strWords[0][0];
-			char[] scramble = new char[strSecret.length()];
-			
-			
 		}
 	}
 }
