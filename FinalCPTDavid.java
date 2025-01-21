f03585a140bb5f9905d130d0e5f3d0d8bc284dc2
davidl-222 <david.lin27@ycdsbk12.ca>
2025-01-20 22:50:53 -0500

    jan 20

--- FinalCPTDavid.java
+++ FinalCPTDavid.java
@@ -1,31 +1,35 @@
 import arc.*;
+import java.awt.*;
+import java.awt.image.*;
 
 public class FinalCPTDavid {
 	public static void main(String[] args) {
-		Console con = new Console();
+		//creates a 1280x720 console with the name Guess The Word
+		Console con = new Console("Guess The Word", 1280, 720);
+		
+		//imports and draws a start game image
+		BufferedImage guesstheword = con.loadImage("guesstheword.png");
+		con.drawImage(guesstheword, 0, 0);
 		boolean blnQuitGame = false;
+		
+		//creates a text file that reads in the high scores
 		TextOutputFile highscore = new TextOutputFile("highscore.txt");
 		while (blnQuitGame == false) {
-			con.clear();
-			con.println("Press p to Play");
-			con.println();
-			con.println("Press s to View High Score");
-			con.println();
-			con.println("Press q to Quit");
-			con.println();
-			con.println("Press h for Help");
-			con.println();
-			
-			char chrChoice = con.readChar();
+			//main menu
+			char chrChoice = mainMenu(con);
 			boolean blnQuit = false;
 			int intScore = 0;
+			
+			//main menu choice to play
 			if (chrChoice == 'p') {
+				//asks users for name
 				con.clear();
 				con.println("What is your name?");
 				String strName = con.readLine();
-				con.println();
+				con.clear();
 				
 				while (blnQuit == false) {
+					//outputs themes to choose from
 					TextInputFile themes = new TextInputFile("themes.txt");
 					while (themes.eof() == false) {
 						String strTheme = themes.readLine();
@@ -35,6 +39,7 @@ public class FinalCPTDavid {
 					String strPlayTheme = con.readLine();
 					con.clear();
 					
+					//based on chosen theme, give random values and bubble sort
 					String[][] strWords = new String[14][2];
 					if (strPlayTheme.equalsIgnoreCase("baseball")) {
 						TextInputFile baseball = new TextInputFile("baseball.txt");
@@ -69,6 +74,8 @@ public class FinalCPTDavid {
 							}
 						}
 					}
+					
+					//scrambled the secret word
 					String strSecret = strWords[0][0];
 					String strScrambled = "";
 					int intLength = strSecret.length();
@@ -95,17 +102,35 @@ public class FinalCPTDavid {
 						strScrambled += scramble[intCount][0];
 					}
 					
+					//start of gameplay to guess the word
 					for (int intCount = intLength - 4; intCount >= 0; intCount--) {
 						con.println(strScrambled);
 						con.println();
 						con.println(intCount + " tries remaining");
+						
+						//statitan cheat
+						if (strName.equals("statitan")) {
+							con.println();
+							con.println("Cheat activated, you get an extra try");
+							intCount++;
+							con.sleep(2000);
+							con.clear();
+							con.sleep(500);
+							con.println(strScrambled);
+							con.println();
+							con.println(intCount + " tries remaining");
+						}
 						con.println();
+						
+						//for the case that the player fails to guess the word
 						if (intCount == 0) {
 							con.println("You didn't guess the word right");
 							con.println("The word was: " + strSecret);
-							con.println("Do you want to PLAY AGAIN or QUIT?");
+							con.println();
+							con.println("Enter p to Play Again");
+							con.println("Enter q to Quit");
 							String strChoice = con.readLine();
-							if (strChoice.equalsIgnoreCase("PLAY AGAIN")) {
+							if (strChoice.equalsIgnoreCase("p")) {
 								con.clear();
 								break;
 							}
@@ -118,16 +143,21 @@ public class FinalCPTDavid {
 						}
 						String strGuess = con.readLine();
 						con.println();
+						
+						//for the case that the player guesses the word right
 						if (strGuess.equalsIgnoreCase(strSecret)) {
 							con.println("You got the word right!");
 							intScore++;
-							con.println("Do you want to PLAY AGAIN or QUIT?");
+							con.println();
+							con.println("Enter p to Play Again");
+							con.println("Enter q to Quit");
 							String strChoice = con.readLine();
-							if (strChoice.equalsIgnoreCase("PLAY AGAIN")) {
+							if (strChoice.equalsIgnoreCase("p")) {
 								con.clear();
 								break;
 							}
 							else {
+								//adds to the players high score
 								highscore.println(intScore + " " + strName);
 								blnQuit = true;
 								con.clear();
@@ -139,35 +169,51 @@ public class FinalCPTDavid {
 					}
 				}
 			}
+			
+			//if the player wants to quit they enter q
 			else if (chrChoice == 'q') System.exit(0);
+			
+			//if the player wants to see the high scores they enter s
 			else if (chrChoice == 's') {
 				con.clear();
 				int intLength = 0;
+				
+				//create a text input file
 				TextInputFile viewscore = new TextInputFile("highscore.txt");
 				while (viewscore.eof() == false) {
 					String strScore = viewscore.readLine();
 					intLength++;
 				}
+				viewscore.close();
+				
+				//reads in the text file into the string array
+				TextInputFile viewscore2 = new TextInputFile("highscore.txt");
 				String[] ordered = new String[intLength];
 				for (int intCount = 0; intCount < intLength; intCount++) {
-					ordered[intCount] = viewscore.readLine();
+					ordered[intCount] = viewscore2.readLine();
 				}
+				
+				//bubble sort by the score
 				String strScoreTemp;	
 				for (int intCount = 0; intCount < intLength - 1; intCount++) {
-					char chrFirst = ordered[intCount].charAt(0);
-					char chrSecond = ordered[intCount+1].charAt(0);
-					if (chrFirst < chrSecond) {
-						strScoreTemp = ordered[intCount];
-						ordered[intCount] = ordered[intCount+1];
-						ordered[intCount+1] = strScoreTemp;
+					for (int i = 0; i < intLength - 1 - intCount; i++) {
+						char chrFirst = ordered[i].charAt(0);
+						char chrSecond = ordered[i+1].charAt(0);
+						if (chrFirst < chrSecond) {
+							strScoreTemp = ordered[i];
+							ordered[i] = ordered[i+1];
+							ordered[i+1] = strScoreTemp;
+						}
 					}
 				}
 				for (int intCount = 0; intCount < intLength; intCount++) {
-					con.println("1. " + ordered[intCount]);
+					con.println(intCount+1 + ". " + ordered[intCount]);
 				}
-				con.sleep(10000);
+				con.sleep(7500);
 				viewscore.close();
 			}
+			
+			//if the player needs help how to play they enter h
 			else if (chrChoice == 'h') {
 				con.clear();
 				con.println("How to play?");
@@ -176,8 +222,24 @@ public class FinalCPTDavid {
 				con.println("A word will be given to you but scrambled");
 				con.println("Then you will have the length of the word - 4 tries to guess the word correctly");
 				con.println("If you get the word right, plus 1 to your highscore until you choose to quit");
-				con.sleep(8500);
+				con.println("Psssst. Make your name statitan ;)");
+				con.sleep(10000);
 			}
 		}
+	}
+	public static char mainMenu(Console con) {
+		//main menu
+		con.clear();
+		con.println("Press p to Play");
+		con.println();
+		con.println("Press s to View High Score");
+		con.println();
+		con.println("Press q to Quit");
+		con.println();
+		con.println("Press h for Help");
+		con.println();
+		
+		char chrChoice = con.getChar();
+		return chrChoice;
 	}
 }
